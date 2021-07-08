046b3d2195152a43125e28788023dc6cc7b85870
solidados <geekbrains.pk@internet.ru>
2021-07-08 23:01:35 +0300

    task-3 appears on the html-page

--- lesson_1/expression.js
+++ lesson_1/expression.js
@@ -2,6 +2,6 @@ let a = 1000;
 let b = '108';
 var c = a + b;
 
-document.write('Переменная <b>a</b> – это ' + typeof a + '<br />');
-document.write('Переменная <b>b</b> – это ' + typeof b + '<br />');
-document.write('Результат суммы этих переменных составляет: ' + '"' + c + '",' + ' и имеет тип – ' + typeof c);
+document.getElementById("expression").value = `Переменная a – это ${typeof a}
+Переменная b – это ${typeof b}
+Результат суммы этих переменных имеет тип – ${typeof c} и составляет: ${c}`;
--- lesson_1/temp.js
+++
@@ -1,4 +0,0 @@
-var celsDeg = propt('Enter degree in Celsius');
-var farhDeg;
-farhDeg = (9 / 5) * celsDeg + 32;
-console.log(farhDeg);
