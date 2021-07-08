4a62451b7fc0853d717609380256372ad73d51fd
Pavel <81189948+solidados@users.noreply.github.com>
2021-07-08 22:10:02 +0300

    homework no.1 for JS-basics

---
+++ lesson_1/expression.js
@@ -0,0 +1,7 @@
+let a = 1000;
+let b = '108';
+var c = a + b;
+
+document.write('Переменная <b>a</b> – это ' + typeof a + '<br />');
+document.write('Переменная <b>b</b> – это ' + typeof b + '<br />');
+document.write('Результат суммы этих переменных составляет: ' + '"' + c + '",' + ' и имеет тип – ' + typeof c);
